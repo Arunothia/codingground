550880499d9a9ee512921f95682039118bc28e42
Arunothia <arunothia@gmail.com>
2016-06-01 12:23:01 +1000

    Using Data.Heap Package for Priority queue implementation as they implement min-heaps based on leftist heaps

--- PCE/data-accessor-0.2.2.7/src/Data/Accessor/MonadState.hs
+++
@@ -1,7 +0,0 @@
-{- | Access helper functions in a State monad -}
-module Data.Accessor.MonadState
-   {-# DEPRECATED "please use Data.Accessor.Monad.Trans.State from data-accessor-transformers" #-}
-   (module Data.Accessor.MonadStatePrivate)
-   where
-
-import Data.Accessor.MonadStatePrivate
