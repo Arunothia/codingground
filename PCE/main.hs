8f4a968987bc78127fd01fff15d76467c0382bf3
Arunothia <arunothia@gmail.com>
2016-06-07 23:11:47 +1000

    paMeet working, tested

--- PCE/main.hs
+++ PCE/main.hs
@@ -2,8 +2,11 @@ module Main where
 import AI.Surely
 import PCE
 
+readInt :: IO [Int]
+readInt = fmap (map read.words) getLine
+
 main = do
-	putStrLn "Enter \n0: No Debug \n1: Debug paTop\n2: Debug assign"
+	putStrLn "Enter \n1: Debug paTop\n2: Debug assign\n3: Debug paMeet\nANY OTHER NUMBER: No Debug"
 	debugInput <- getLine
 	let debug = read debugInput :: Int
 	putStrLn "Enter value of Vocabulary 'n'"
@@ -16,4 +19,9 @@ main = do
 		     	lInput <- getLine
 		     	let l = read lInput :: Int
 		     	print $ assign n l
+	else if debug == 3
+		then do
+			putStrLn "Enter 2 Literals (space seperated) for checking paMeet (we use assign to check it)"
+			[p,q] <- readInt
+			print $ paMeet (assign n p) (assign n q)
 	else return()
