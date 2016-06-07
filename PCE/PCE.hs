38175014b2dc991edd70067800144ce4d95ac8ae
Arunothia <arunothia@gmail.com>
2016-06-07 22:55:37 +1000

    Partial Assignment model changed to Maybe [PAValue], some basic testing added in main.hs

--- PCE/PCE.hs
+++ PCE/PCE.hs
@@ -2,125 +2,78 @@
 -- Written by M.Arunothia as a part of research internship under Prof.Harald, University of Melbourne.
 -----------------------------------------------------------------------------------------------------------
 module PCE where
--- For Looping Constructs
-import Data.IORef 
 -- For SAT Solver
 import AI.Surely 
 -- For Priority Queue 
 import Data.Heap 
--- For Fix
-import Data.Function 
 -- For fromJust Function :: Maybe a -> a
 import Data.Maybe
 -- For find Function for List manipulation
 import Data.List
 
 -----------------------------------------------------------------------------------------------------------
--- Looping Constructs as given in http://www.xoltar.org/old_site/2003//sep/09/haskellLoops.html
 
--- Define 'foreach'
+-- Data Type PAValue helps to define our partial assignment. It can be (True|False|Question|Contradiction).
+-- The value 'contradiction' helps in identifying contradictions in the assignment of that variable.
 
---foreach = flip mapM_
-
--- Define 'while'
-
---while test action = do
-
---  val <- test
-
---  if val then do {action;while test action}
-
---         else return ()
-
--- Some helpers for use with 'while':
-
---incr ref = modifyIORef ref (+1)
-
---test ref f = do { val <- readIORef ref; return (f val) }
-
--- Exercise them. Equivalent Python code:
-
--- for x in range(1,11): 
-
---     print x
-
--- i = 0
-
--- while i < 5:
-
---     print "Still running"
-
---     i += 1
-
-
-
--- main = do
-
---  foreach [1..10] print
-
---  ref <- newIORef 0
-
---  while (test ref (< 5))
-
---        (do 
-
---	  print "Still running!"
-
---          incr ref)
------------------------------------------------------------------------------------------------------------
-
--- Data Type PAValue helps to define our partial assignment. It can be (True|False|Question), exactly the range of partial assignments.
-
-data PAValue = PAFalse | PATrue | PAQuest
+data PAValue = PAFalse | PATrue | PAQuest | PAContra
 		deriving (Eq, Show)
 
--- An instance of Ord is to be written for Partial Assignments that will enable the functioning of priority queues. To be completed.
+-----------------------------------------------------------------------------------------------------------
 
--- isQues and isFalse function do trivially what their name suggests
+-- paTop is the fully unidentified partial assignment.
+-- Partial Assignment in this code is defined by the data type Maybe [PAValue]
 
-isQues :: PAValue -> Bool
-isQues v = if (v == PAQuest) then True else False
+paTop :: Int -> Maybe ([PAValue])
+paTop 0 = Nothing
+paTop n = Just (replicate n PAQuest)
 
-isFalse :: PAValue -> Bool
-isFalse v = if (v == PAFalse) then True else False
+-----------------------------------------------------------------------------------------------------------
 
--- paTop Function takes any integer to a question. That is, the function is fully unidentified.
-
-paTop :: Int -> PAValue
-paTop v = PAQuest
-
-
--- assign(l) Function defines a partial assignment when a literal is specified.
+-- assign Function defines a partial assignment when a literal is specified.
+-- Its first integer input stands for 'n' - that denotes the vocabulary.
 -- It takes an integer (negative for negated variables) and returns a partial assignment.
 -- When the integer passed is zero (which should'nt be the case), the partial assignment returned marks all literals false.
  
-assign :: Int -> (Int -> PAValue) 
-assign 0 = \x-> PATrue
-assign l
-	| (l>0) = \x -> if (x == l) then PATrue else PAQuest
-	| otherwise = \x -> if (x == l) then PAFalse else PAQuest
-
+assign :: Int -> Int -> Maybe [PAValue]
+assign n 0 = Just (replicate n PAFalse)
+assign 0 _ = Nothing
+assign n l
+	| (l > 0) = Just(init(first) ++ [PATrue] ++ second) 
+	| otherwise = Just(init(first) ++ [PAFalse] ++ second)
+	where  (first,second) = Data.List.splitAt (abs(l)) (fromJust(paTop n))
+		  
 -- paMeet Function defines the 'meet' operator for combining two partial assignments.
 -- If the two partial assignments lead to a contradiction, then we return Nothing (denoting contradiction).
 -- Otherwise we return the unification of the two partial assignments.
 
-paMeet :: (Int -> PAValue) -> (Int -> PAValue) -> Maybe (Int -> PAValue)
-paMeet p q = Nothing -- To be completed.
+------------------------------------------------------------------------------------------------------------
+
+paMeet :: Maybe [PAValue] -> Maybe [PAValue] -> Maybe [PAValue]
+paMeet _ Nothing = Nothing
+paMeet Nothing _ = Nothing
+paMeet (Just p) (Just q) = if length(Prelude.filter (== PAContra) unifiedPA) > 0 then Nothing else Just unifiedPA  
+	where unifiedPA = zipWith unifyPA p q
+	      unifyPA a b
+		| (a == b) = a
+		| (a == PAQuest) = b
+		| (b == PAQuest) = a
+		| otherwise = PAContra
 
 -- up(c) Function defines a unit propagation function that takes a partial assignment to another partial assignment.
 -- It takes a list of integers (that represents a clause) as input.
 
-up :: [Int] -> (Int -> PAValue) -> Maybe (Int -> PAValue)
-up c = \p -> if ((length(Prelude.filter isQues (map p c)) == 1) && (length(Prelude.filter isFalse (map p c)) == length(c) - 1)) 
-	     	then paMeet p (assign (fromJust (find (isQues.p) c)))
-		else Just p
+up :: [Int] -> Maybe [PAValue] -> Maybe [PAValue]
+up _ Nothing  = Nothing
+up c (Just p) = Nothing -- To be completed
 
 -- gfpUP Function - Greatest Fixed Point of applying up(c) for each clause in the encoding.
 -- It takes a set of clauses (List of list of integers) and a partial assignment as the input.
 -- It outputs the GFP of unit propagation as mentioned in the paper
 
-gfpUP :: [[Int]] -> (Int -> PAValue) -> Maybe (Int -> PAValue)
-gfpUP setC p = Nothing -- To be completed.
+gfpUP :: [[Int]] -> Maybe [PAValue] -> Maybe [PAValue]
+gfpUP _ Nothing     = Nothing
+gfpUP setC (Just p) = Nothing -- To be completed.
 
 -- pce is the function that will function as our Algorithm-1
 -- PCE - stands for Propagation Complete Encodings
@@ -141,7 +94,4 @@ gfpUP setC p = Nothing -- To be completed.
 
 pce :: Int -> [[Int]] -> [[Int]] -> [[Int]]
 pce n eIni eRef = eIni
-	--where 
-	  --  heapPQ = Data.Heap.fromList [] :: MinHeap (Int -> PAValue)
-	   -- ins = Data.Heap.insert paTop heapPQ
-	   -- e = [] -- To be completed.
+
--- PCE/main.hs
+++ PCE/main.hs
@@ -1,4 +1,19 @@
 module Main where
 import AI.Surely
+import PCE
 
-main = putStrLn $ show $ solve [[1,2],[-1,3],[-3]]
+main = do
+	putStrLn "Enter \n0: No Debug \n1: Debug paTop\n2: Debug assign"
+	debugInput <- getLine
+	let debug = read debugInput :: Int
+	putStrLn "Enter value of Vocabulary 'n'"
+	nInput <- getLine
+	let n = read nInput :: Int
+	if debug == 1 then print $ PCE.paTop n
+	else if debug == 2 
+		then do
+			putStrLn "Enter Literal value for checking assign" 
+		     	lInput <- getLine
+		     	let l = read lInput :: Int
+		     	print $ assign n l
+	else return()
